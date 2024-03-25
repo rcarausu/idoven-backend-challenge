154805b352f50724282469d870834a7813defdf6
rcarausu <robertcarausu@gmail.com>
2024-03-25 18:09:53 +0100

    Version 0.0.1
    Implemented basic register ECG and insights endpoints.

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 
 from src.ecg.adapter.in_adapters.web.main import api_router
 
-app = FastAPI()
+app = FastAPI(
+    title="ECG app",
+    summary="Register and check insights on electrocardiograms (ECGs)",
+    version="0.0.1",
+    contact={
+        "name": "Robert Carausu",
+        "url": "https://www.linkedin.com/in/robertcarausu/",
+    },
+)
 
 app.include_router(api_router)
-
-
-
